997dabaa4b4ff80bfd6ef55ea132cb13771cd4ea
Mariusz Politowicz <politowiczmar@gmail.com>
2023-01-04 19:11:43 +0100

    Add cart list

--- src/js/script.js
+++ src/js/script.js
@@ -402,6 +402,9 @@
       thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(
         select.cart.toggleTrigger
       );
+      thisCart.dom.productList = thisCart.dom.wrapper.querySelector(
+        select.cart.productList
+      );
     }
     initActions() {
       const thisCart = this;
@@ -411,8 +414,14 @@
       });
     }
     add(menuProduct) {
-      // const thisCart = this;
-
+      const thisCart = this;
+      /* generate HTML based on template */
+      const generatedHTML = templates.cartProduct(menuProduct);
+      /* create DOM element using utils.createElementFromHTML */
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+      console.log(generatedDOM);
+      /* add element to menu */
+      thisCart.dom.productList.appendChild(generatedDOM);
       console.log('adding product', menuProduct);
     }
   }
