0252cf1ffed290a82dc3bcd5e2206a30ea67fca0
Mariusz Politowicz <politowiczmar@gmail.com>
2023-01-15 01:15:32 +0100

    Add booking class

---
+++ src/js/components/Booking.js
@@ -0,0 +1,36 @@
+import { select, templates } from '../settings.js';
+import utils from '../utils.js';
+import AmountWidget from '../components/AmountWidget.js';
+
+class Booking {
+  constructor(element) {
+    const thisBooking = this;
+
+    thisBooking.render(element);
+    thisBooking.initWidgets();
+  }
+  render(element) {
+    const thisBooking = this;
+
+    const generatedHTML = templates.bookingWidget();
+    thisBooking.element = utils.createDOMFromHTML(generatedHTML);
+    const bookingContainer = document.querySelector(select.containerOf.booking);
+    bookingContainer.appendChild(thisBooking.element).innerHTML;
+
+    thisBooking.dom = {
+      wrapper: element,
+      peopleAmount: element.querySelector(select.booking.peopleAmount),
+      hoursAmount: element.querySelector(select.booking.hoursAmount),
+    };
+  }
+  initWidgets() {
+    const thisBooking = this;
+
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.dom.peopleAmount.addEventListener('updated', function () {});
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.dom.hoursAmount.addEventListener('updated', function () {});
+  }
+}
+
+export default Booking;
